27e9a445d9971e1a49fa1a8e167e0fb4c0ae51f6
carlitoxd <clmg1010@gmail.com>
2014-10-26 22:07:44 -0430

    creacion de multiples rectangulos que se mueven. cada rectanglo tiene un id. cuando se mueven se hace una llamada con ajax.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,157 @@
 
+
+console.log("Creando Variables Iniciales");
+
+
+
+// Declarcion de Variables
+   var i =0; // Simulando la generacion de IDS
+   var escenario;
    var principal;
-   var rect;
-   
    var JSONposiciones = [ ];
 
-   function agregar () {
 
-   	console.log("Iniciando");
-    
-      // creacion del escenario
-    var escenario = new Kinetic.Stage({
-        container: 'myCanvas',
-        width:  600,
-        height:  500,
+
+
+
+
+    // creacion del escenario
+    escenario = new Kinetic.Stage({
+    	container: 'myCanvas',
+    	width:  600,
+    	height:  500,
     });
- 
-
-     principal = new Kinetic.Layer();
 
 
-	    // creacion de un rectangulo
-    rect = new Kinetic.Rect({
+    principal = new Kinetic.Layer();
+
+
+   // agregando nuevo layer
+   escenario.add(principal);
+   escenario.draw();
+
+
+
+ // Funcion para Crear Rectangulos
+ function addRect () {
+
+
+ 	var rectangulo = new Kinetic.Rect({
+ 		x: 200,
+ 		y: 0,
+ 		width: 120,
+ 		height: 80,
+ 		offset: [50, 35],
+ 		fill: 'skyblue',
+ 		stroke: 'lightgray',
+ 		strokeWidth: 2,
+ 		draggable: true,
+ 		id: ""+i++
+ 	});
+
+    	  // agregando evento cuando presione Abajo
+    	  window.addEventListener('keydown', function (e) {
+
+
+
+    	  	if (e.keyCode == "40") {
+    	  		console.log("Abajo");
+    	  		rectangulo.rotateDeg(30);
+    	  		principal.draw();
+
+    	  	}
+   // console.log(e.keyCode)
+}, true);
+
+
+  // agregando evento cuando presione Arriba
+  window.addEventListener('keyup', function (e) {
+
+  	if (e.keyCode == "38") {
+  		console.log("Arriba");
+  		rectangulo.rotateDeg(-30);
+  		principal.draw(); 
+
+  	}
+   // console.log(shiftPressed)
+}, true);
+
+
+     // evento cuando le den click para que gire  
+     rectangulo.on('click', function() { 
+
+	   //  console.log(principal);
+	   this.rotateDeg(30);
+	   principal.draw();
+
+
+	   console.log('Rotando: '+'x: ' + this.getPosition().x + ', y: ' +  this.getPosition().y);
+	});
+
+
+	// evento para cuando se mueva el layer
+	rectangulo.on('dragmove', function(e) {
+
+		//if  (rect.isDragging() || principal.isDragging()) {
+
+	   //console.log('Moviendose: '+'x: ' + rect.getPosition().x + ', y: ' +  rect.getPosition().y);
+	   var json =  '[{"id":'+this.getId() +' ,"position": {"x":'+this.getPosition().x+', "y":'+this.getPosition().y+'}}]';
+
+
+	   console.log('Moviendose: '+json);
+	  // Se realiza el ajax
+
+	  $.ajax({
+	  	type: "POST",
+	  	url: "http://localhost/canvas.php",
+	  	data: "data",
+	  	success: "",
+	  	dataType: ""
+	  });
+
+		//}// fin del if 
+
+
+	});
+
+
+
+      // one revolution per 4 seconds
+        var angularSpeed = 360 / 4;
+        var anim = new Kinetic.Animation(function(frame) {
+          var angleDiff = frame.timeDiff * angularSpeed / 1000;
+          rectangulo.rotate(angleDiff);
+   
+        }, principal);
+
+        anim.start();
+
+        
+	    	// creacion de un rectangulo
+	    	principal.add( rectangulo);
+	    	principal.draw();
+
+
+
+	 
+
+
+	    };
+
+
+
+
+
+
+
+// Funcion para agregar
+function agregar () {
+
+
+
+
+// creacion de un rectangulo
+   /* rect = new Kinetic.Rect({
 	    x: 150,
 	    y: 100,
 	    width: 120,
@@ -30,62 +161,29 @@
     	stroke: 'lightgray',
 	    strokeWidth: 2,
 	    draggable: true
-	});
-	 
+	});*/
+
+
+
 	// 
- 	principal.add(rect);
-    escenario.add(principal);
-    escenario.draw();
+ 	//principal.add(rect);
 
 
-      var canvas = escenario.getContainer();
+
+      //var canvas = escenario.getContainer();
 	  //console.log(canvas)
-        
-  // evento cuando le den click para que gire  
- principal.on('click', function() { 
-
-      	rect.rotateDeg(30);
-         principal.draw();
 
 
-        console.log('Moviendose: '+'x: ' + rect.getPosition().x + ', y: ' +  rect.getPosition().y);
-});
-
-
-// evento para cuando se mueva el layer
-principal.on('dragmove', function(e) {
-
-	if  (rect.isDragging() || principal.isDragging()) {
-
-   //console.log('Moviendose: '+'x: ' + rect.getPosition().x + ', y: ' +  rect.getPosition().y);
-   var json =  '[{"positionInicio": {"x":'+rect.getPosition().x+', "y":'+rect.getPosition().y+'}}]';
-
-
-   console.log('Moviendose: '+json);
-  
-  $.ajax({
-type: "POST",
-url: "http://localhost/canvas.php",
-data: "data",
-success: "",
-dataType: ""
-});
-	
-	} 
- 
-
-
-});
 
 
   // evento cuando inicie el drag
-principal.on('dragstart', function() {
+  principal.on('dragstart', function() {
 
 /*		if  (rect.isDragging() || principal.isDragging()) {
 
    console.log('position mouse on canvas: '+'x: ' + rect.getPosition().x + ', y: ' +  rect.getPosition().y);
     console.log ("es drag");
-	} */
+} */
 //console.log("dragstart",rect.getPosition());
 //J
 
@@ -108,7 +206,7 @@ dataType: ""
 });
 
   // evento cuando finalice el drag
-principal.on('dragend', function() {
+  principal.on('dragend', function() {
 //console.log("dragend",rect.getPosition());
 //posiciones.push (rect.getPosition());
 //var json =  '[{"positionFin": {"x":'+rect.getPosition().x+', "y":'+rect.getPosition().y+'}}]';
@@ -120,36 +218,7 @@ principal.on('dragend', function() {
 
 
 
-var shiftPressed
-  // agregando evento cuando presione Abajo
-window.addEventListener('keydown', function (e) {
-    
-     
-  
-    if (e.keyCode == "40") {
-    	 console.log("Abajo");
-        shiftPressed = true;
-         rect.rotateDeg(30);
-         principal.draw();
-
-    }
-   // console.log(e.keyCode)
-}, true);
 
 
-  // agregando evento cuando presione Arriba
-window.addEventListener('keyup', function (e) {
- 
-    if (e.keyCode == "38") {
-       shiftPressed = false;
-       console.log("Arriba");
-	rect.rotateDeg(-30);
-    principal.draw(); 
-      
-   }
-   // console.log(shiftPressed)
-}, true);
-
-   
- };
+};
 
