41d35d2797bb13b99231de443dd2f87ceb241c02
carlitoxd <clmg1010@gmail.com>
2014-10-27 00:40:24 -0430

    Simulacion de dos usuarios.En el panel superior el usuario A realiza los movimientos..
    En el panel de abajo el usuario B solo puede ver los movimientos del A

--- js/app.js
+++ js/app.js
@@ -12,10 +12,8 @@ console.log("Creando Variables Iniciales");
    var indexAnterior=0;
    var escenario;
    var principal;
-   var JSONposiciones = [ ];
+   var JSONArreglos = [ ];
    var colores = ['#D24D57','#446CB3', '#03C9A9', 'skyblue', '#EB974E' ,'#D35400' ,'grey'];
-
-
 
 
 
@@ -26,13 +24,11 @@ console.log("Creando Variables Iniciales");
     	height:  300,
     });
 
-
     principal = new Kinetic.Layer();
 
-
-   // agregando nuevo layer
-   escenario.add(principal);
-   escenario.draw();
+  	 // agregando nuevo layer
+   	 escenario.add(principal);
+  	 escenario.draw();
 
 
 
@@ -66,6 +62,8 @@ console.log("Creando Variables Iniciales");
  		id: ""+i++
  	});
 
+
+
  	indexAnterior = index;
 
     	  // agregando evento cuando presione Abajo
@@ -88,27 +86,31 @@ console.log("Creando Variables Iniciales");
 
   	if (e.keyCode == "38") {
   		console.log("Arriba");
-  		rectangulo.rotateDeg(-40);
+  		rectangulo.rotateDeg(-20);
   		principal.draw(); 
 
   	}
    // console.log(shiftPressed)
 }, true);
 
+    
+
 
      // evento cuando le den click para que gire  
      rectangulo.on('click', function() { 
 
 	   //  console.log(principal);
-	   this.rotateDeg(40);
+	   this.rotateDeg(20);
 	   principal.draw();
+         console.log(this);
 
-
-	   console.log(' click  Rotando: '+'x: ' + this.getPosition().x + ', y: ' +  this.getPosition().y);
+	   	   //console.log('Moviendose: '+'x: ' + rect.getPosition().x + ', y: ' +  rect.getPosition().y);
+	     var json =  '[  {"id":'+this.getId() +' , "rotation":'+this.getRotation() +' , "width":'+this.getWidth() +' ,	 "height":'+this.getHeight() +' ,    "fill":'+this.getFill() +' ,    "stroke":'+this.getStroke() +' ,	  "strokeWidth":'+this.getStrokeWidth() +' ,	"draggable":'+false +' ,"position": {"x":'+this.getPosition().x+', "y":'+this.getPosition().y+'}}]';
+         JSONArreglos[this.getId()]= json;
 	});
 
 
-
+ 
 
 
 	// evento para cuando se mueva el layer
@@ -116,11 +118,13 @@ console.log("Creando Variables Iniciales");
 
 		//if  (rect.isDragging() || principal.isDragging()) {
 
+
+
 	   //console.log('Moviendose: '+'x: ' + rect.getPosition().x + ', y: ' +  rect.getPosition().y);
-	   var json =  '[{"id":'+this.getId() +' ,"position": {"x":'+this.getPosition().x+', "y":'+this.getPosition().y+'}}]';
+	     var json =  '[  {"id":'+this.getId() +' , "rotation":'+this.getRotation() +' , "width":'+this.getWidth() +' ,	 "height":'+this.getHeight() +' ,    "fill":'+this.getFill() +' ,    "stroke":'+this.getStroke() +' ,	  "strokeWidth":'+this.getStrokeWidth() +' ,	"draggable":'+false +' ,"position": {"x":'+this.getPosition().x+', "y":'+this.getPosition().y+'}}]';
+         JSONArreglos[this.getId()]= json;
 
-
-	   console.log('Moviendose: '+json);
+	   console.log('Moviendose: '+JSONArreglos);
 	  // Se realiza el ajax
 
 	  $.ajax({
@@ -148,6 +152,8 @@ console.log("Creando Variables Iniciales");
              
             rectangulo.setY(vectorY);
              vectorY+=1;
+
+
              
              if (rectangulo.getY() == escenario.getHeight() - rectangulo.getHeight()) {
 
@@ -155,11 +161,15 @@ console.log("Creando Variables Iniciales");
 					//addRect ();
 
              }
+             	 var json =  '[  {"id":'+rectangulo.getId() +', "rotation":'+rectangulo.getRotation() +' , "width":'+rectangulo.getWidth() +' ,	 "height":'+rectangulo.getHeight() +' ,    "fill":"'+rectangulo.getFill() +'"  ,    "stroke":"'+rectangulo.getStroke() +'"  ,	  "strokeWidth":'+rectangulo.getStrokeWidth() +' ,	"draggable":'+false +' ,"position": {"x":'+rectangulo.getPosition().x+', "y":'+rectangulo.getPosition().y+'}}]';
+                 JSONArreglos[rectangulo.getId()]= json;
+
+                console.log(JSONArreglos);
         }, principal);
 
         anim.start();
 
-
+             
 	    	// creacion de un rectangulo
 	    	principal.add( rectangulo);
 	    	principal.draw();
@@ -262,4 +272,70 @@ dataType: ""
 
 
 };
+
+//---------------------PANEL PARA EL SEGUNDO USUARIO------------------------------------------------------------
+
+  
+
+   
+// Funcion para agregar
+function refresh () {
+
+	   // creacion del escenario
+    var escenario2 = new Kinetic.Stage({
+    	container: 'OtheUserCanvas',
+    	width:  600,
+    	height:  300,
+    });
+
+
+	 var principal2 = new Kinetic.Layer();
+
+  	 // agregando nuevo layer
+   	 escenario2.add(principal2);
+  	 escenario2.draw();
+
+
+    
+
+
+ for (var i = 0; i < JSONArreglos.length; i++) {
+ 
+ 	
+var obj = jQuery.parseJSON(JSONArreglos[i]);
+	console.log(obj[0].width) ;
+ 	   var rectangulo = new Kinetic.Rect({
+ 		x:   obj[0].position.x,
+ 		y:  obj[0].position.y,
+ 		width: obj[0].width,
+ 		height: obj[0].height,
+ 		rotation: obj[0].rotation,
+ 		offset: [50, 35],
+ 		fill: obj[0].fill,
+ 		stroke: obj[0].stroke,
+ 		strokeWidth:obj[0].strokeWidth,
+ 		draggable:obj[0].draggable,
+ 		id:obj[0].id, 
+ 	});
+
+ 	   	// creacion de un rectangulo
+	    	principal2.add( rectangulo);
+	    	principal2.draw();
+
+
+ };
+
+
+
+
+
+
+  }
+   
+
+
+
+window.setInterval(function(){
+refresh ()
+}, 500);
 
